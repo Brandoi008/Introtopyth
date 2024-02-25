95c96e50dce19e5c7cbe4175b3b74b50b8065124
Brandoi008 <makutobrian1@gmail.com>
2024-02-25 05:19:45 -0400

    first commit

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# user name
+name = input("What is your name?")
+
+# user age
+age = input("What is your age?")
+
+# user location
+location = input("Where do you live?")
+
+
+# Print message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
